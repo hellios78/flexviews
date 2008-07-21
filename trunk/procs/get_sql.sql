fe81067bf33ae00b99270126b066482548fa2b85
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2008-07-21 08:32:05 +0000

    add new function to display the SQL for the specified incremental refresh materialized view.  need to add support for complete views

---
+++ trunk/procs/get_sql.sql
@@ -0,0 +1,42 @@
+DELIMITER ;;
+/*  Flexviews for MySQL
+    Copyright 2008 Justin Swanhart
+
+    FlexViews is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FlexViews is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FlexViews in the file COPYING, and the Lesser extension to
+    the GPL (the LGPL) in COPYING.LESSER.
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+
+DROP FUNCTION IF EXISTS flexviews.get_sql;;
+
+CREATE DEFINER=flexviews@localhost FUNCTION flexviews.get_sql (
+  v_mview_id INT
+)
+RETURNS TEXT
+READS SQL DATA
+BEGIN
+  DECLARE v_sql TEXT default '';
+
+  SET v_sql = CONCAT(flexviews.get_select(v_mview_id, 'CREATE',''), char(10));
+  SET v_sql = CONCAT(v_sql, flexviews.get_from(v_mview_id, '\nJOIN', ''));
+  SET v_sql = CONCAT(v_sql, flexviews.get_where(v_mview_id));
+
+  SET v_sql = CONCAT(v_sql, IF(flexviews.has_aggregates(v_mview_id) = true, '\nGROUP BY ', ''), flexviews.get_delta_groupby(v_mview_id), ' ');
+
+  RETURN v_sql;
+
+END;
+;;
+
+DELIMITER ; 
