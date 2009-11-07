dc9c72f2d0b29796ceac52884105ce6fdd212721
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2009-11-07 08:09:03 +0000

    Remove the patch to mysqlbinlog.c  - the LUA processor (wip) will supercede it.

--- lua/consumer.lua
+++ lua/consumer.lua
@@ -1,3 +1,116 @@
+
+--[[
+   Author: Julio Manuel Fernandez-Diaz
+   Date:   January 12, 2007
+   (For Lua 5.1)
+   
+   Modified slightly by RiciLake to avoid the unnecessary table traversal in tablecount()
+
+   Formats tables with cycles recursively to any depth.
+   The output is returned as a string.
+   References to other tables are shown as values.
+   Self references are indicated.
+
+   The string returned is "Lua code", which can be procesed
+   (in the case in which indent is composed by spaces or "--").
+   Userdata and function keys and values are shown as strings,
+   which logically are exactly not equivalent to the original code.
+
+   This routine can serve for pretty formating tables with
+   proper indentations, apart from printing them:
+
+      print(table.show(t, "t"))   -- a typical use
+   
+   Heavily based on "Saving tables with cycles", PIL2, p. 113.
+
+   Arguments:
+      t is the table.
+      name is the name of the table (optional)
+      indent is a first indentation (optional).
+--]]
+function table.show(t, name, indent)
+   local cart     -- a container
+   local autoref  -- for self references
+
+   --[[ counts the number of elements in a table
+   local function tablecount(t)
+      local n = 0
+      for _, _ in pairs(t) do n = n+1 end
+      return n
+   end
+   ]]
+   -- (RiciLake) returns true if the table is empty
+   local function isemptytable(t) return next(t) == nil end
+
+   local function basicSerialize (o)
+      local so = tostring(o)
+      if type(o) == "function" then
+         local info = debug.getinfo(o, "S")
+         -- info.name is nil because o is not a calling level
+         if info.what == "C" then
+            return string.format("%q", so .. ", C function")
+         else 
+            -- the information is defined through lines
+            return string.format("%q", so .. ", defined in (" ..
+                info.linedefined .. "-" .. info.lastlinedefined ..
+                ")" .. info.source)
+         end
+      elseif type(o) == "number" then
+         return so
+      else
+         return string.format("%q", so)
+      end
+   end
+
+   local function addtocart (value, name, indent, saved, field)
+      indent = indent or ""
+      saved = saved or {}
+      field = field or name
+
+      cart = cart .. indent .. field
+
+      if type(value) ~= "table" then
+         cart = cart .. " = " .. basicSerialize(value) .. ";\n"
+      else
+         if saved[value] then
+            cart = cart .. " = {}; -- " .. saved[value] 
+                        .. " (self reference)\n"
+            autoref = autoref ..  name .. " = " .. saved[value] .. ";\n"
+         else
+            saved[value] = name
+            --if tablecount(value) == 0 then
+            if isemptytable(value) then
+               cart = cart .. " = {};\n"
+            else
+               cart = cart .. " = {\n"
+               for k, v in pairs(value) do
+                  k = basicSerialize(k)
+                  local fname = string.format("%s[%s]", name, k)
+                  field = string.format("[%s]", k)
+                  -- three spaces between levels
+                  addtocart(v, fname, indent .. "   ", saved, field)
+               end
+               cart = cart .. indent .. "};\n"
+            end
+         end
+      end
+   end
+
+   name = name or "__unnamed__"
+   if type(t) ~= "table" then
+      return name .. " = " .. basicSerialize(t)
+   end
+   cart, autoref = "", ""
+   addtocart(t, name, indent)
+   return cart .. autoref
+end
+
+
+
+
+
+
+
 --[[ $%BEGINLICENSE%$
  Copyright (C) 2008 MySQL AB, 2008 Sun Microsystems, Inc
 
@@ -16,9 +129,46 @@
 
  $%ENDLICENSE%$ --]]
 local binlog = assert(require("mysql.binlog"))
--- require"std"
+local luamysql = assert(require("luasql.mysql"))
+local db = assert(luamysql.mysql());
 
-local logfile = "/tmp/rbr.bin"
+-- TODO: make this read from a config file
+conn = assert(db:connect("flexviews", "root", "flexroot", "127.0.0.1", 3307))
+conn:setautocommit(true)
+
+-- retrieve a cursor
+cur = assert (conn:execute"SHOW BINARY LOGS;")
+-- print all rows, the rows will be indexed by field names
+row = cur:fetch ({}, "n")
+while row do
+  -- print(table.show(row))
+  --  print(string.format("Name: %s", row.log_name))
+  -- reusing the table of results
+  
+  print(string.format("INSERT INTO flexviews.binlog_consumer_status (master_log_file, master_log_size, exec_master_pos) values ('%s', %d, 0) ON DUPLICATE KEY UPDATE master_log_size = %d ;", row[1], row[2], row[2]))
+  row = cur:fetch (row, "n")
+end
+-- close the cursor
+cur:close()
+
+
+
+-- retrieve a cursor
+cur = assert (conn:execute"SELECT bcs.*, setting_value from flexviews.binlog_consumer_status bcs join flexviews.mview_settings where setting_key ='log_bin' and exec_master_pos < master_log_size order by master_log_file;")
+-- print all rows, the rows will be indexed by field names
+row = cur:fetch ({}, "n")
+while row do
+  print(string.format("PROCESS BINARY LOG: %s%s, size:%d, exec_at:%d, exec_to:%d", row[4], row[1], row[2], row[3], row[2]))
+  row = cur:fetch (row, "n")
+end
+-- close the cursor
+cur:close()
+conn:close()
+db:close()
+
+
+function process_binlog(logfile) 
+ -- local logfile = "/tmp/rbr.bin"
 local f = assert(binlog.open(logfile))
 
 for event in f:next() do
@@ -127,3 +277,12 @@ for event in f:next() do
 end
 
 f:close()
+
+
+end -- FUNCTION
+
+
+
+-- [PROGRAM ENDS HERE]
+
+
