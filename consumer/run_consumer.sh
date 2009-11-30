9cd5a189fbd31822d7ab1562e057a8cb80e60781
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2009-11-30 22:44:33 +0000

    The consumer now backs off retrying to process logs if there were no logs to process in the previous run.  Each time no new logs are detected, 250ms is added to the wait time.

--- consumer/run_consumer.sh
+++ consumer/run_consumer.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+SLEEP_TIME=10
+
 while [ 1 ]
 do
 	php consumer.php
-        usleep 500000 
+	if [ $? -eq 0 ]; then
+          let SLEEP_TIME=SLEEP_TIME+250000       
+        else
+          let SLEEP_TIME=10;
+        fi;
+
+        if [ $SLEEP_TIME -gt 5000000 ]; then
+          let SLEEP_TIME = 5000000;
+        fi;
+        echo "$SLEEP_TIME";
+        usleep $SLEEP_TIME;
 done
 
