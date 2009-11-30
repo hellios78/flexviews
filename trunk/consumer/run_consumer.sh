9ad777af0432ae1b70bc445bc210d3ff25a5cfd1
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2009-11-30 23:58:57 +0000

    Add support for loading an arbitary .ini file w/ the command line.  Default is still "./consumer.ini".

--- trunk/consumer/run_consumer.sh
+++ trunk/consumer/run_consumer.sh
@@ -3,7 +3,7 @@ SLEEP_TIME=10
 
 while [ 1 ]
 do
-	php consumer.php
+	php consumer.php ./consumer.ini
 	if [ $? -eq 0 ]; then
           let SLEEP_TIME=SLEEP_TIME+250000       
         else
@@ -13,7 +13,6 @@ do
         if [ $SLEEP_TIME -gt 5000000 ]; then
           let SLEEP_TIME = 5000000;
         fi;
-        echo "$SLEEP_TIME";
         usleep $SLEEP_TIME;
 done
 
