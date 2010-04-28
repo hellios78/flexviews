9ec4376a030f29a1dc4c9b677da68ce1ce15133b
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2010-04-28 18:12:13 +0000

    Further update of the class.  Remove most function parameters and replace with class members.  Add new function to update the binary log position, instead of using UPDATE in multiple places.

--- consumer/consumer.php
+++ consumer/consumer.php
@@ -2,11 +2,6 @@
 <?php
 error_reporting(E_ALL);
 
-class FlexApplier {
-
-}
-
-
 class FlexConsumer {
 	private	$mvlogList = array();
 	private $settings = array();
@@ -18,7 +13,9 @@ class FlexConsumer {
 
 	private $serverId = NULL;
 
-		
+	#Construct a new consumer object.
+	#By default read settings from the INI file unless they are passed
+	#into the constructor	
 	function __construct($settings = NULL) {
 		if($settings) {
 			$this->settings = $settings;	
@@ -83,12 +80,16 @@ class FlexConsumer {
 		$processedLogs = 0;
 		while($row = mysql_fetch_assoc($stmt)) {
 			++$processedLogs;
-
+		
 			if ($row['exec_master_log_pos'] < 4) $row['exec_master_log_pos'] = 4;
 			$execCmdLine = sprintf("%s --base64-output=decode-rows -v -R --start-position=%d --stop-position=%d %s", $this->cmdLine, $row['exec_master_log_pos'], $row['master_log_size'], $row['master_log_file']);
 			echo  "-- $execCmdLine\n";
 			$proc = popen($execCmdLine, "r");
+			$this->binlogPosition = $row['exec_master_log_pos'];
+			$this->logName = $row['master_log_file'];
 			$this->process_binlog($proc, $row['master_log_file'], $row['exec_master_log_pos']);
+			#make sure the end of the binary log is captured
+			$this->set_capture_pos();
 			pclose($proc);
 		}
 		
@@ -112,10 +113,7 @@ class FlexConsumer {
 		mysql_query("BEGIN;", $this->dest) or die(mysql_error());
 		mysql_query("CREATE TEMPORARY table flexviews.log_list (log_name char(50), primary key(log_name))",$this->dest) or die(mysql_error());
 	
-		$sql = "SELECT @@server_id";
-		$stmt = mysql_query($sql, $this->source);
-		$row = mysql_fetch_array($stmt) or die($sql . "\n" . mysql_error() . "\n");
-		$this->serverId = $row[0];
+
 	
 		$stmt = mysql_query("SET SQL_LOG_BIN=0", $this->dest);
 		if(!$stmt) die(mysql_error());
@@ -126,8 +124,16 @@ class FlexConsumer {
 		
 	}
 	
-	/* Get the list of logs from the source */
+	/* Get the list of logs from the source and place them into a temporary table on the dest*/
 	function get_source_logs() {
+		/* This server id is not related to the server_id in the log.  It refers to the ID of the 
+		 * machine we are reading logs from.
+		 */
+		$sql = "SELECT @@server_id";
+		$stmt = mysql_query($sql, $this->source);
+		$row = mysql_fetch_array($stmt) or die($sql . "\n" . mysql_error() . "\n");
+		$this->serverId = $row[0];
+		
 		$stmt = mysql_query("SHOW BINARY LOGS", $this->source);
 		if(!$stmt) die(mysql_error());
 	
@@ -150,15 +156,17 @@ class FlexConsumer {
 		mysql_query($sql, $this->dest) or die("Could not drop TEMPORARY TABLE log_list\n");
 	}
 
+	function set_capture_pos() {
+		$sql = sprintf("UPDATE flexviews.binlog_consumer_status set exec_master_log_pos = %d where master_log_file = '%s' and server_id = %d",$this->serverId, $this->binlogPosition, $this->logName);
+		mysql_query($sql, $dest) or die("COULD NOT EXEC:\n$sql\n" . mysql_error());
+		
+	}
 
 	/* Called when a new transaction starts*/
-	function start_transaction($binlogPosition, $timeStamp, $logName) {
+	function start_transaction() {
 		mysql_query("START TRANSACTION", $this->dest) or die("COULD NOT START TRANSACTION;\n" . mysql_error());
-
-		$sql = sprintf("UPDATE flexviews.binlog_consumer_status set exec_master_log_pos = %d where master_log_file = '%s' and server_id = {$this->serverId}", $binlogPosition, $logName);
-		mysql_query($sql, $dest) or die("COULD NOT EXEC:\n$sql\n" . mysql_error());
-
-		$sql = sprintf("INSERT INTO flexviews.mview_uow values(NULL,str_to_date('%s', '%%y%%m%%d %%H:%%i:%%s'));",rtrim($timeStamp));
+        $this->set_capture_pos();
+		$sql = sprintf("INSERT INTO flexviews.mview_uow values(NULL,str_to_date('%s', '%%y%%m%%d %%H:%%i:%%s'));",rtrim($this->timeStamp));
 		mysql_query($sql,$dest) or die("COULD NOT CREATE NEW UNIT OF WORK:\n$sql\n" .  mysql_error());
 		 
 		$sql = "SET @fv_uow_id := LAST_INSERT_ID();";
@@ -167,138 +175,134 @@ class FlexConsumer {
 	}
 
     /* Called when a transaction commits */
-	function commit_transaction($binlogPosition, $timeStamp, $logName) {
-		$sql = sprintf("UPDATE flexviews.binlog_consumer_status set exec_master_log_pos = %d where master_log_file = '%s' and server_id = {$this->serverId}", $binlogPosition, $logName);
-		mysql_query($sql, $dest) or die("COULD NOT EXEC:\n$sql\n" . mysql_error());
+	function commit_transaction() {
+		$this->set_capture_pos();
 		mysql_query("COMMIT", $this->dest) or die("COULD NOT COMMIT TRANSACTION;\n" . mysql_error());
 	}
 
 	/* Called when a transaction rolls back */
-	function rollback_transaction($binlogPostion, $timeStamp, $logName) {
+	function rollback_transaction() {
 		mysql_query("ROLLBACK", $this->dest) or die("COULD NOT ROLLBACK TRANSACTION;\n" . mysql_error());
-		$sql = sprintf("UPDATE flexviews.binlog_consumer_status set exec_master_log_pos = %d where master_log_file = '%s' and server_id = {$this->serverId}", $binlogPosition, $logName);
-		mysql_query($sql, $dest) or die("COULD NOT EXEC:\n$sql\n" . mysql_error());
+		#update the capture position and commit, because we don't want to keep reading a rolled back 
+		#transaction in the log, if it is the last thing in the log
+		$this->set_capture_pos();
 		mysql_query("COMMIT", $this->dest) or die("COULD NOT COMMIT TRANSACTION LOG POSITION UPDATE;\n" . mysql_error());
 		
 	}
 
-	/* Called when a row is deleted */
+	/* Called when a row is deleted, or for the old image of an UPDATE */
 	function delete_row($mvLogDB, $mvLogTable, $row = array()) {
 		$valList .= "(1, @fv_uow_id, {$this->serverId}," . implode(",", $row) . ")";
 		$sql = sprintf("INSERT INTO %s.`%s` VALUES %s", $mvLogDB, $mvLogTable, $valList );
 		mysql_query($sql, $dest) or die("COULD NOT EXEC SQL:\n$sql\n" . mysql_error());
 	}
 
-	/* Called when a row is inserted */
+	/* Called when a row is inserted, or for the new image of an UPDATE */
 	function insert_row($mvLogDB, $mvLogTable, $row = array()) {
 		$valList .= "(1, @fv_uow_id, $this->serverId," . implode(",", $row) . ")";
 		$sql = sprintf("INSERT INTO %s.`%s` VALUES %s", $mvLogDB, $mvLogTable, $valList );
 		mysql_query($sql, $this->dest) or die("COULD NOT EXEC SQL:\n$sql\n" . mysql_error());
 	}
 	
-	function process_binlog($proc,$logName, $binlogPosition) {
+	function process_binlog($proc) {
 		static $mvlogDB = false;
-		//$newTransaction = true;
-		/*
-		 global $mvlogList;
-		 global $source, $dest;
-		 global $serverId;
-		 */
-		$timeStamp = false;
+		$this->timeStamp = false;
 
 		$this->refresh_mvlog_cache();
 		$sql = "";
 
-		$valList = "";
-		$oldTable = "";
-
-
-
 		$lastLine = "";
 
+		#read from the mysqlbinlog process one line at a time.
+		#note the $lastLine variable - we process rowchange events
+		#in another procedure which also reads from $proc, and we
+		#can't seek backwards, so this function returns the next line t3o process
+		#In this case we use that line instead of reading from the file again
 		while( !feof($proc) ) {
 			if($lastLine) {
+				#use a previously saved line (from process_rowlog)
 				$line = $lastLine;
 				$lastLine = "";
 			} else {
+				#read from the process
 				$line = trim(fgets($proc));
 			}
 
 			#echo "-- $line\n";
-
+			#It is faster to check substr of the line than to run regex
+			#on each line.
 			$prefix=substr($line, 0, 5);
-
 			$matches = array();
-
 
 			if($prefix == "### I" || $prefix == "### U" || $prefix == "### D") {
 				if(preg_match('/### (UPDATE|INSERT INTO|DELETE FROM)\s([^.]+)\.(.*$)/', $line, $matches)) {
-					$db = $matches[2];
-					$table = $matches[3] . '_mvlog';
-					$base_table = $matches[3];
+					$this->db          = $matches[2];
+					$this->mvlog_table = $matches[3] . '_mvlog';
+					$this->base_table  = $matches[3];
 
-					if($db == 'flexviews' && $table == 'mvlogs') {
+					if($this->db == 'flexviews' && $this->base_table == 'mvlogs') {
 						refresh_mvlog_cache();
 					}
 
-					if(!empty($mvlogList[$db . $base_table])) {
-						$lastLine = process_rowlog($proc, $db, $table, $serverId, $this->mvlogDB);
+					if(!empty($this->mvlogList[$db . $base_table])) {
+						$lastLine = process_rowlog($proc);
 						continue;
 					}
 				}
 			} else {
-				if(preg_match('/^#([0-9: ]+).*\s+end_log_pos ([0-9]+)/', $line,$matches)) {
+				if($prefix[0] == "#" && preg_match('/^#([0-9: ]+).*\s+end_log_pos ([0-9]+)/', $line,$matches)) {
 
-					$binlogPosition = $matches[2];
-					if(!$timeStamp) {
-						new_uow($binlogPosition, $matches[1], $serverId, $logName);
+					$this->binlogPosition = $matches[2];
+					if(!$this->timeStamp) {
+						$this->start_transaction();
 					}
-					$timeStamp = $matches[1];
+					$this->timeStamp = $matches[1];
 				}
 			}
 			 
 
 			if($prefix=="# End" || (substr($line, 0, 6) == "COMMIT"))  {
-				new_uow($binlogPosition, $timeStamp, $serverId, $logName);
+				$this->commit_transaction();
 			}
 
 		}
 		 
 	}	
 	
-
-	function process_rowlog($proc, &$db, &$table, &$serverId, &$mvLogDB) {
+	function process_rowlog($proc) {
 		$sql = "";
-		$valList = "";
 		$line = "";
-		global $onlyDatabases;
-		global $source, $dest;
 		$skip_rows = false;
 
 		#if there is a list of databases, and this database is not on the list
 		#then skip the rows
-		if(!empty($this->onlyDatabases) && empty($this->onlyDatabases[trim($db)])) {
+		if(!empty($this->onlyDatabases) && empty($this->onlyDatabases[trim($this->db)])) {
 			$skip_rows = true;
 		}
 
 		# loop over the input, collecting all the input values into a set of INSERT statements
-		$values = array();
 		$row = array();
 		$mode = 0;
 		
 		while($line = fgets($proc)) {
 			#echo "***$line";
 			$line = trim($line);
+			
+            #DELETE and UPDATE statements contain a WHERE clause with the OLD row image
 			if($line == "### WHERE") {
 				$mode = -1;
+				
+			#INSERT and UPDATE statements contain a SET clause with the NEW row image
 			} elseif($line == "### SET")  {
-				if ($valList) {
-					$sql .= $valList . ")";
-					if(!$skip_rows) mysql_query($sql, $dest) or die("COULD NOT EXEC SQL:\n$sql\n" . mysql_error());
-				}
 				$mode = 1;
+			
+			#Row images are in format @1 = 'abc'
+			#                         @2 = 'def'
+			#Where @1, @2 are the column number in the table	
 			} elseif(preg_match('/###\s+@[0-9]+=(.*)$/', $line, $matches)) {
 				$row[] = $val;
+
+			#This line does not start with ### so we are at the end of the images	
 			} else {
 				if(!$skip_rows) {
 					switch($mode) {
@@ -313,8 +317,9 @@ class FlexConsumer {
 					}					
 				} 
 				$row = array();
-				break;
+				break; #out of while
 			}
+			#keep reading lines
 		}
 		#return the last line so that we can process it in the parent body
 		#you can't seek backwards in a proc stream...
