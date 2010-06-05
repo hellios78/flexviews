8d2e03b30e776a85f4d1574f2cc9c8c5994946be
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2010-06-05 21:25:07 +0000

    Improvements to the setup process.  Include feedback about completion, etc

--- consumer/setup.php
+++ consumer/setup.php
@@ -5,6 +5,8 @@ $cdc = new FlexCDC();
 
 #this will read settings from the INI file and initialize
 #the database and capture the source master position
+echo "setup starting\n";
 $cdc->setup(true);
+echo "setup completed\n";
 
 ?>
