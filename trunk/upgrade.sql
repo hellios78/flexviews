f77604a8e0f3c1f074fd30c7538753bc843677ae
swany <swany@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2010-09-07 21:01:23 +0000

    Add an example my.cnf to the test suite.  Finish the upgrade process.

--- trunk/upgrade.sql
+++ trunk/upgrade.sql
@@ -1,19 +1,27 @@
-create database if not exists flexviews;
+# This script only works if you are running 1.5.3b3.  If you are running an older verions, uninstall and reinstall
 use flexviews;
 
-create table if not exists upgrades (
-  version int,
-  proc_name varchar(20) not null, 
-  primary key (version)
-) engine = innodb;
 
-create table if not exists version (
-  version int,
-  primary key (version)
-) engine = innodb;
+select 'Adding new aggregate function types to the flexviews metadata' as '';
+#add new aggregate function types.  note that, stddev is deprecated - stddev_pop is now used instead
+ALTER TABLE mview_expression 
+            MODIFY `mview_expr_type` enum('GROUP','SUM','AVG','COUNT','MIN','MAX','WHERE','PRIMARY','KEY','COLUMN','COUNT_DISTINCT','STDDEV','STDDEV_POP','VAR_POP','STDDEV_SAMP','VAR_SAMP','BIT_AND','BIT_OR','BIT_XOR', 'PERCENTILE','UNIQUE') DEFAULT 'GROUP', 
+            ADD percentile tinyint default null;
 
--- register the availability of the upgrade package
-replace into upgrades values ('160','upgrade_160');
+select 'Update any STDDEV expressions to be STDDEV_POP expression (same result, different name)' as '';
+#fix any STDDEV to be STDDEV_POP
+UPDATE mview_expression set mview_expr_type = 'STDDEV_POP' WHERE mview_expr_type='STDDEV';
 
- 
+select 'Removing ability to add new STDDEV expressions (use STDDEV_POP)' as '';
+#remove STDDEV as an available option
+ALTER TABLE mview_expression 
+            MODIFY `mview_expr_type` enum('GROUP','SUM','AVG','COUNT','MIN','MAX','WHERE','PRIMARY','KEY','COLUMN','COUNT_DISTINCT','STDDEV_POP','VAR_POP','STDDEV_SAMP','VAR_SAMP','BIT_AND','BIT_OR','BIT_XOR', 'PERCENTILE','UNIQUE') DEFAULT 'GROUP'; 
+
+select 'Installing stored procedures' as '';
+\. install_procs.inc
+select 'upgrade done.' as '';
+
+
+
+
 
