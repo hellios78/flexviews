a90ff020d725e7be44698fdc61a46440b6c10ee7
greenlion <greenlion@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2011-03-06 01:57:25 +0000

    revise upgrade process

--- trunk/upgrade.sql
+++ trunk/upgrade.sql
@@ -1,8 +1,6 @@
 # This script only works if you are running 1.6.0rc1.  If you are running an older verions, uninstall and reinstall
 use flexviews;
 
-ALTER IGNORE TABLE flexviews.mivew_table add unique (mview_id, mview_table_alias);
-
 select 'Installing stored procedures' as '';
 \. install_procs.inc
 
