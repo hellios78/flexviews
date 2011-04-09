4d99cc449576510ceecb18f8e5e410a2aefdf125
greenlion <greenlion@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2011-04-09 20:29:39 +0000

    Improve documentation of upgrade process

--- trunk/upgrade.sql
+++ trunk/upgrade.sql
@@ -1,4 +1,4 @@
-# This script only works if you are running 1.6.0rc1.  If you are running an older verions, uninstall and reinstall
+# This script only works if you are running 1.6.0rc1 or greater.  If you are running an older verions, uninstall and reinstall
 use flexviews;
 
 select 'Installing stored procedures' as '';
