6cef016b382d15d87c4b6614d53eddd3019c2f75
greenlion <greenlion@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2012-07-02 03:13:42 +0000

    minor changes for beta release

--- upgrade.sql
+++ upgrade.sql
@@ -1,7 +1,5 @@
 # This script only works if you are running 1.6.0rc1 or greater.  If you are running an older verions, uninstall and reinstall
 use flexviews;
 
-select 'Installing stored procedures' as '';
-\. install_procs.inc
+select 'Upgrade not available!\nCall flexviews.disable() for all your views, then drop the flexviews database and follow normal install procedures' as '';
 
-select 'upgrade done.' as '';
