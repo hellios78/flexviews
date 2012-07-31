456a0e59d74fbd2e3a05acc0b55e32a1d3d5f16b
greenlion <greenlion@a032f43c-3b7d-ab7d-bbc0-f2f7014d6bbb>
2012-07-31 01:52:59 +0000

    Improve memory usage, fix problems with wrapper scripts (logging works properly)

--- consumer/consumer_safe.sh
+++ consumer/consumer_safe.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  php run_consumer.php --pid=flexcdc.pid $* > /dev/null 2>&1 < /dev/null
+  php run_consumer.php --pid=flexcdc.pid $* > flexcdc.err 2>&1 < /dev/null
   if [ $? -eq 0 ]; then
     exit 0
   fi
